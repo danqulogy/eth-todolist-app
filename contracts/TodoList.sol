17363313cc17a30b8666c3b24e315f55d957c805
Bernard D. White <danquahwhite@gmail.com>
2019-04-22 11:48:58 +0000

    Solved the issue of contract not working on network

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -10,6 +10,12 @@ contract TodoList {
 
     uint public taskCount = 0;
     mapping (uint => Task) public tasks;
+
+    constructor() public{
+       createTask("Complete Wetula Platform");
+    }
+
+
     
     function createTask(string memory content) public {
         uint taskId = taskCount++;
@@ -17,4 +23,12 @@ contract TodoList {
         Task memory task = Task({id: taskId, content: content, completed: false });
         tasks[taskId] = task;
     }
+
+    function getTaskByID(uint _id) view public returns (string memory) {
+        require(tasks[_id].id != 0, "Task does not exist ");
+
+        return tasks[_id].content;
+    }
+
+   
 }
