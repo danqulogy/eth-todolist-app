7300d8805e76a5a0c08cdce50a398707e21be9ae
Bernard D. White <danquahwhite@gmail.com>
2019-04-22 05:39:09 +0000

    Implemented CreateTask method

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,7 +1,20 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
-    uint public todoCount = 0;
 
+    struct Task{
+        uint id;
+        string content;
+        bool completed;
+    }
 
+    uint public taskCount = 0;
+    mapping (uint => Task) public tasks;
+    
+    function createTask(string memory content) public {
+        uint taskId = taskCount++;
+
+        Task memory task = Task({id: taskId, content: content, completed: false });
+        tasks[taskId] = task;
+    }
 }
